9805753a237979dbbb1982c7cef3f653aa5073d3
BRajendra10 <rajendrabehera8116@gmail.com>
2026-05-31 09:56:04 +0530

    ...

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -215,7 +215,7 @@ const login = asyncHandler(async (req, res) => {
         .cookie("accessToken", accessToken, accessTokenOptions)
         .cookie("refreshToken", refreshToken, refreshTokenOptions)
         .json(
-            new ApiResponse(200, {}, "Login successfully")
+            new ApiResponse(200, safeUser, "Login successfully")
         )
 });
 
